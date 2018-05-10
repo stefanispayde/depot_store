d6a006919db01a486bae43ca0f10307ed020bd77
Stefani Spayde <stefani@bearandgiraffe.com>
2018-05-10 14:45:04 -0500

    created new migration and new up/down methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_10_112550) do
+ActiveRecord::Schema.define(version: 2018_05_10_193934) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
